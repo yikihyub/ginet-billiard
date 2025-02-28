f058f0ffb41380876df5dfefdab6e3255cb87656
yikihyub <kihyub123123@naver.com>
2025-02-28 15:21:18 +0900

    bcryptjs설치치

--- src/app/api/(auth)/signin/route.ts
+++ src/app/api/(auth)/signin/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 
 export async function POST(request: NextRequest) {
   try {
